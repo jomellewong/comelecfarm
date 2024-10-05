7067ba16f1f3047b5f13a4b28fdd5613ded2b8fe
jomellewong <159461111+jomellewong@users.noreply.github.com>
2024-10-05 22:09:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-print("Hello, World!")
+import urllib.request
+import json
+
+baseurl = "https://2022electionresults.comelec.gov.ph/"
+url = "https://2022electionresults.comelec.gov.ph/data/regions/44/44021.json"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+req = urllib.request.Request(url, None, headers)
+
+with urllib.request.urlopen(req) as file:
+#   regions json
+    region = json.load(file)
+
+    for r_key in region['srs']:
+        req = urllib.request.Request(
+            baseurl + "data/regions/" + region['srs'][r_key]['url'] + ".json", 
+            None, headers
+            )
+        
+        with urllib.request.urlopen(req) as file:
+#           provinces json
+            province = json.load(file)
+            
+            for p_key in province['srs']:
+                req = urllib.request.Request(
+                    baseurl + "data/regions/" + province['srs'][p_key]['url'] + ".json",
+                    None, headers
+                )
+
+                with urllib.request.urlopen(req) as file:
+                    citymun = json.load(file)
+
+                    for cm_key in citymun['srs']:
+                        req = urllib.request.Request(
+                            baseurl + "data/regions/" + citymun['srs'][cm_key]['url'] + ".json",
+                            None, headers
+                        )
+
+                        with urllib.request.urlopen(req) as file:
+                            brgy = json.load(file)
+
+                            for b_key in brgy['srs']:
+                                req = urllib.request.Request(
+                                    baseurl + "data/regions/" + brgy['srs'][b_key]['url'] + ".json",
+                                    None, headers
+                                )
+
+                                with urllib.request.urlopen(req) as file:
+                                    precinct = json.load(file)
+
+                                    for pct in precinct['pps']:
+                                        req = urllib.request.Request(
+                                            baseurl + "data/results/" + pct['vbs']['url'] + ".json",
+                                            None, headers
+                                        )
+
+                                        with urllib.request.urlopen(req) as file:
+                                            result = json.load(file)
